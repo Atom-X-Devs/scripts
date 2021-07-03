e95e464333a9ba1969cce9826485cfbd948617fd
Pig <pig.priv@gmail.com>
2021-07-03 16:17:43 +0200

    Scripts: Initial script for kernel module updates or import
    Change-Id: I19ffbee9acdf1dbe412a18d14553f1cd7163b20f

---
+++ tfa-import.py
@@ -0,0 +1,13 @@
+import os
+
+def mergeCAF(x,path):
+    os.system("git fetch "+x)
+    os.system("git merge -s ours --no-commit --allow-unrelated-histories FETCH_HEAD")
+    os.system("git read-tree --prefix="+path+" -u FETCH_HEAD")
+    mas = path+": Initial tfa98xx Driver Import from CAF "+branch
+    os.system("git commit -s -m '"+mas+"'")
+
+branch = input("Enter tfa98xx branch: ")
+tfa98xx = "http://source.codeaurora.org/external/mas/tfa98xx "+branch
+
+mergeCAF(tfa98xx,"techpack/audio/asoc/codecs/tfa9874")
