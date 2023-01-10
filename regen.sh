399619b4f681f2eeaf21481216d0543181d0ed0f
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-01-11 01:04:29 +0600

    regen: Bump to `v3.3`
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- regen.sh
+++ regen.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: GPL-3.0
 # Copyright (c) 2022-2023, Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>.
-# Revision: 09-01-2023 V3.2
+# Revision: 11-01-2023 V3.3
 
 # Variables for colors
 RED="\033[1;31m"
